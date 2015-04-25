d4e8100b6938fda5f5aadbc859419a41cf1ec560
spolispastom <kurenkovmichael@gmail.com>
2015-04-25 12:46:49 +0400

    edgdsfg
    sdfsdf

--- WebSites/WebSite6/Default.aspx
+++ WebSites/WebSite6/Default.aspx
@@ -10,28 +10,44 @@
 
         <br />
 
-      <asp:Label ID="Label5" runat="server" Text="Поиск грантов и стипендий" CssClass="container" Font-Size="20pt"></asp:Label> 
+   
+        <asp:Table ID="Table2" runat="server" Width="820px">
+            <asp:TableRow runat="server">
+                <asp:TableCell runat="server">   <asp:Label ID="Label5" runat="server" Text="Поиск грантов и стипендий" CssClass="container" Font-Size="20pt"></asp:Label> </asp:TableCell>
+                <asp:TableCell runat="server"  HorizontalAlign="Left" Width="50"><asp:Label ID="Label7" runat="server"  Text="Академическая  мобильность" ForeColor="#6F2F00" Font-Bold="True" Font-Size="13pt" Width="220px"></asp:Label></asp:TableCell>
+            </asp:TableRow>
+         
+        </asp:Table>
+ 
+        
 
-        <br />
-&nbsp;&nbsp;
+    
 
-      <br />	
-        <asp:Table ID="Table1" runat="server" Height="130px" Width="61px" CellPadding="5" CellSpacing="5" BorderStyle="None">
+
+
+        <asp:Table ID="Table1" runat="server" Height="130px" Width="61px" CellPadding="5" CellSpacing="5" BorderStyle="None" ForeColor="#414141">
            
             <asp:TableRow runat="server" BorderStyle="Double" BorderColor="#333333" BorderWidth="2px" ClientIDMode="Static">
 
                 <asp:TableCell runat="server"><asp:Label ID="Label1" runat="server" Text="Страна"></asp:Label>
 </asp:TableCell>
-                <asp:TableCell runat="server"><asp:ListBox ID="ListBox1" runat="server" DataSourceID="XmlDataSource1" DataValueField="" 
-                            DataMember="countrie" DataTextField="name"  Height="90px" Width="180px" Font-Size="Medium"></asp:ListBox>
-</asp:TableCell>
+               
                 <asp:TableCell ID="Cell1" runat="server" RowSpan="20" VerticalAlign="Top"></asp:TableCell>
 
+            </asp:TableRow>
+
+              <asp:TableRow ID="TableRow1" runat="server">
+                   <asp:TableCell ID="TableCell1" runat="server"><asp:ListBox ID="ListBox1" runat="server" DataSourceID="XmlDataSource1" DataValueField="" 
+                            DataMember="countrie" DataTextField="name"  Height="90px" Width="180px" Font-Size="Medium"></asp:ListBox>
+</asp:TableCell>
             </asp:TableRow>
             <asp:TableRow runat="server" BorderColor="#333333" BorderStyle="Groove" BorderWidth="2px">
                 <asp:TableCell runat="server"><asp:Label ID="Label2" runat="server" Text="Вид"></asp:Label>
 </asp:TableCell>
-                <asp:TableCell runat="server"><asp:ListBox ID="ListBox2" runat="server" Height="90px" Width="180px" Font-Size="Medium"><asp:ListItem>Летние школы</asp:ListItem>
+                
+            </asp:TableRow>
+             <asp:TableRow ID="TableRow2" runat="server">
+                 <asp:TableCell ID="TableCell2" runat="server"><asp:ListBox ID="ListBox2" runat="server" Height="90px" Width="180px" Font-Size="Medium"><asp:ListItem>Летние школы</asp:ListItem>
 <asp:ListItem>Магистратура</asp:ListItem>
 <asp:ListItem>Докторантура</asp:ListItem>
 <asp:ListItem>Семинары</asp:ListItem>
@@ -46,22 +62,35 @@
             <asp:TableRow runat="server">
                 <asp:TableCell runat="server"><asp:Label ID="Label4" runat="server" Text="Направление"></asp:Label>
 </asp:TableCell>
-                <asp:TableCell runat="server"><asp:ListBox ID="ListBox3" runat="server" Height="39px"   Width="180px" Font-Size="Medium"><asp:ListItem>экономическое</asp:ListItem>
+             
+            </asp:TableRow> 
+                <asp:TableRow ID="TableRow3" runat="server">
+                       <asp:TableCell ID="TableCell3" runat="server"><asp:ListBox ID="ListBox3" runat="server" Height="39px"   Width="180px" Font-Size="Medium"><asp:ListItem>экономическое</asp:ListItem>
 <asp:ListItem>техническое</asp:ListItem>
 </asp:ListBox>
 </asp:TableCell>
             </asp:TableRow>
             <asp:TableRow runat="server">
-                <asp:TableCell runat="server" HorizontalAlign="Right"><asp:Button ID="Button3" runat="server"  BackColor="#6666FF" BorderStyle="None" Font-Bold="False" Font-Size="16px" ForeColor="White" Text="Найти" OnClick="Button3_Click"  />
+                <asp:TableCell runat="server" HorizontalAlign="Center"><asp:Button ID="Button3" runat="server"  BackColor="#6666FF" BorderStyle="None" Font-Bold="False" Font-Size="16px" ForeColor="White" Text="Найти" OnClick="Button3_Click"  />
+&nbsp;<asp:Button ID="Button1" runat="server" BackColor="#999999" BorderStyle="None" Font-Bold="False" Font-Size="16px" ForeColor="White" OnClick="Button1_Click" Text="Очистить" Font-Overline="False" BorderColor="Gray" BorderWidth="10"   />
 </asp:TableCell>
-                <asp:TableCell runat="server"><asp:Button ID="Button1" runat="server" BackColor="#999999" BorderStyle="None" Font-Bold="False" Font-Size="16px" ForeColor="White" OnClick="Button1_Click" Text="Очистить" Font-Overline="False" BorderColor="Gray" BorderWidth="10"   />
-</asp:TableCell>
+              
+
             </asp:TableRow>
             <asp:TableRow runat="server">
-                <asp:TableCell runat="server" Height="200px"></asp:TableCell>
+                <asp:TableCell runat="server" Height="200px"><asp:Label ID="Label6" runat="server" Text="Показаны 10 случайных вариантов. Рекомендуем сузить условия поиска: выберите хотя бы страну." Font-Size="10" Font-Bold="False" Visible="False"></asp:Label></asp:TableCell>
                 <asp:TableCell runat="server" Height="200px"></asp:TableCell>
             </asp:TableRow>
+        
+           
+          
         </asp:Table>
+
+     
+        
+
+     
+        
 
      
         
--- WebSites/WebSite6/Default.aspx.cs
+++ WebSites/WebSite6/Default.aspx.cs
@@ -20,6 +20,7 @@ public partial class _Default : Page
         ListBox1.ClearSelection();
         ListBox2.ClearSelection();
         ListBox3.ClearSelection();
+        Label6.Visible = false; 
     }
    
     protected void Button3_Click(object sender, EventArgs e)
@@ -27,92 +28,105 @@ public partial class _Default : Page
     string country = ListBox1.SelectedValue;
     string type = ListBox2.SelectedIndex.ToString();
     string spec = ListBox3.SelectedIndex.ToString();
-    
+  
     string inCountry = "";
     string inType = "";
     string inSpec = "";
     string inDiscription = "";
     string inName = "";
+    Label6.Visible = false;
+    if ((country == "") && (type == "-1") && (spec == "-1"))
+    {
+        Label6.Visible = true;
+    }
     xml.Load(HttpContext.Current.Server.MapPath("~/App_Data/grants.xml"));
         Table tbl = new Table();
+        int k = 10;
+        bool[] flags = new bool[9];
     foreach (XmlNode n in xml.SelectNodes("/grants/grant")) 
             {
-         
+        
             
              inCountry= n.SelectSingleNode("country").InnerText;
              inType = n.SelectSingleNode("type").InnerText;
              inSpec = n.SelectSingleNode("spec").InnerText;
-        if (((inCountry == country)||(country == ""))&&((inType == type)||(type == "-1"))&&((inSpec == spec)||(spec == "-1"))) 
-            {Label l = new Label();
-             Label l2 = new Label();
-             l.ForeColor = Color.FromArgb(44, 60, 112);
-             l.Font.Bold = true;
-             Table tblIn = new Table();
-             HyperLink hl = new HyperLink();
-             TableRow tbrOut = new TableRow();
-             TableRow tbr = new TableRow();
-             TableCell tbcOut = new TableCell();
-             TableRow tbr2 = new TableRow();
-             TableRow tbr3 = new TableRow();
-             TableCell tbc3 = new TableCell();
-             TableCell tbc = new TableCell();
-             TableCell tbc2 = new TableCell();
-             WebUserControl wuc = new WebUserControl();
-            
-             tbc3.HorizontalAlign = HorizontalAlign.Right;
-             Button b = new Button();
-             Button b2 = new Button();
-             b.Text = "Сайт гранта";
-             b2.Text = "Сохранить";
-            // b.CssClass = "enjoy_css";
-          b.BorderStyle = BorderStyle.None;
-             b.BackColor = Color.FromArgb(249,171,45);
-             b.Font.Size = FontUnit.Medium;
-            // b.Attributes.Add("class","enjoy_css");
-            inName = n.SelectSingleNode("name").InnerText;
-            inDiscription = n.SelectSingleNode("discription").InnerText;
-            
-             l.Text = inName;
-             l2.Text = inDiscription;
-        
-            // b.PostBackUrl = ;
-           //  b.OnClientClick = "navAway('" + n.SelectSingleNode("url").InnerText + "')";
-             b.Attributes.Add("onclick", "navAway('" + n.SelectSingleNode("url").InnerText + "')");
+             if (!flags[Convert.ToInt32(inType)-1])
+             {
+                 flags[Convert.ToInt32(inType)-1] = true;
 
-         b2.Attributes.Add("onclick", "post('sdfsdf')");
-           
-            // b2.Attributes.Add("onclick", "  VK.callMethod(\"saveWallPost\", \"Хрень \");");
-             hl.NavigateUrl = hl.Text;
-             l.Width = 450;
-             tbc.Style.Add("border-bottom", "2px solid #eee");
-            tbc3.Style.Add("border-top", "2px solid #eee");
-            tbc3.Style.Add("padding", "2px");
-            b.Style.Add("margin", "2px");
-            tblIn.BorderStyle = BorderStyle.Ridge;
+                 if (((inCountry == country) || (country == "")) && ((inType == type) || (type == "-1")) && ((inSpec == spec) || (spec == "-1")))
+                 {
+                     Label l = new Label();
+                     Label l2 = new Label();
+                     l.ForeColor = Color.FromArgb(44, 60, 112);
+                     l.Font.Bold = true;
+                     Table tblIn = new Table();
+                     HyperLink hl = new HyperLink();
+                     TableRow tbrOut = new TableRow();
+                     TableRow tbr = new TableRow();
+                     TableCell tbcOut = new TableCell();
+                     TableRow tbr2 = new TableRow();
+                     TableRow tbr3 = new TableRow();
+                     TableCell tbc3 = new TableCell();
+                     TableCell tbc = new TableCell();
+                     TableCell tbc2 = new TableCell();
+                     WebUserControl wuc = new WebUserControl();
 
-             tblIn.BorderColor = Color.LightGray;
-             tblIn.BorderWidth = 1;
-           
-            tbr.Controls.Add(tbc);
-            tbc.Controls.Add(l);
-            tbr2.Controls.Add(tbc2);
-            tbc2.Controls.Add(l2);
-             // tbc3.Controls.Add(b2);  
-            tbc3.Controls.Add(b);
-             
-            tbr3.Controls.Add(tbc3);
-         //   tbc3.Controls.Add(wuc);
-         
-            tblIn.Controls.Add(tbr);
-            tblIn.Controls.Add(tbr2);
-            tblIn.Controls.Add(tbr3);
-            tbcOut.Controls.Add(tblIn);
-            tbrOut.Controls.Add(tbcOut);
-            tbrOut.BackColor = Color.White;
-            tbl.Controls.Add(tbrOut);
-          
-           
-        }
+                     tbc3.HorizontalAlign = HorizontalAlign.Right;
+                     Button b = new Button();
+                     Button b2 = new Button();
+                     b.Text = "Сайт гранта";
+                     b2.Text = "Сохранить";
+                     // b.CssClass = "enjoy_css";
+                     b.BorderStyle = BorderStyle.None;
+                     b.BackColor = Color.FromArgb(249, 171, 45);
+                     b.Font.Size = FontUnit.Medium;
+                     // b.Attributes.Add("class","enjoy_css");
+                     inName = n.SelectSingleNode("name").InnerText;
+                     inDiscription = n.SelectSingleNode("discription").InnerText;
+
+                     l.Text = inName;
+                     l2.Text = inDiscription;
+
+                     // b.PostBackUrl = ;
+                     //  b.OnClientClick = "navAway('" + n.SelectSingleNode("url").InnerText + "')";
+                     b.Attributes.Add("onclick", "navAway('" + n.SelectSingleNode("url").InnerText + "')");
+
+                     b2.Attributes.Add("onclick", "post('sdfsdf')");
+
+                     // b2.Attributes.Add("onclick", "  VK.callMethod(\"saveWallPost\", \"Хрень \");");
+                     hl.NavigateUrl = hl.Text;
+                     l.Width = 560;
+                     tbc.Style.Add("border-bottom", "2px solid #eee");
+                     tbc3.Style.Add("border-top", "2px solid #eee");
+                     tbc3.Style.Add("padding", "2px");
+                     b.Style.Add("margin", "2px");
+                     tblIn.BorderStyle = BorderStyle.Ridge;
+
+                     tblIn.BorderColor = Color.LightGray;
+                     tblIn.BorderWidth = 1;
+
+                     tbr.Controls.Add(tbc);
+                     tbc.Controls.Add(l);
+                     tbr2.Controls.Add(tbc2);
+                     tbc2.Controls.Add(l2);
+                     // tbc3.Controls.Add(b2);  
+                     tbc3.Controls.Add(b);
+
+                     tbr3.Controls.Add(tbc3);
+                     //   tbc3.Controls.Add(wuc);
+
+                     tblIn.Controls.Add(tbr);
+                     tblIn.Controls.Add(tbr2);
+                     tblIn.Controls.Add(tbr3);
+                     tbcOut.Controls.Add(tblIn);
+                     tbrOut.Controls.Add(tbcOut);
+                     tbrOut.BackColor = Color.White;
+                     tbl.Controls.Add(tbrOut);
+
+
+                 }
+             }
 
             }
     Cell1.Controls.Clear();
@@ -122,6 +136,7 @@ public partial class _Default : Page
     pnl.ScrollBars = ScrollBars.Vertical;
     pnl.Controls.Add(tbl);
     Cell1.Controls.Add(pnl);
+ 
         
  
  
